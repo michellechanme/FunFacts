03c11e6d018a27a72c95f321c874e9184a3254b7
Michelle Chan <michellechan.me@gmail.com>
2015-08-05 15:04:10 -0700

    crashlytics

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         application.setStatusBarStyle(UIStatusBarStyle.LightContent, animated: false)
+        Fabric.with([Crashlytics()])
         return true
     }
 
